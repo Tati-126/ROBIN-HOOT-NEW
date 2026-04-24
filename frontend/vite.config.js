4a85fc879fd4dfc16f01880434640196dc18b2de
fqrid <faridzemanate@gmail.com>
2026-04-24 17:56:49 -0500

    feat(frontend): integrar importacion de trivia desde microservicio

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -11,6 +11,10 @@ export default defineConfig({
                 target: "http://localhost:5000",
                 changeOrigin: true,
             },
+            "/trivia": {
+                target: "http://localhost:5001",
+                changeOrigin: true,
+            },
         },
     },
 });
